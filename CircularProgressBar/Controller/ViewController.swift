b45ceacbe16c780f6c9a61a6abbf8ae4248459d2
Massimiliano <massimiliano@MacBook-Pro-di-Massimiliano.local>
2019-11-13 06:51:33 +0000

    progress bar

---
+++ CircularProgressBar/Controller/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  CircularProgressBar
+//
+//  Created by Massimiliano on 12/11/2019.
+//  Copyright © 2019 Massimiliano Bonafede. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    @IBOutlet var progressBar: ProgressBarView!
+    @IBOutlet var startBtn: UIButton!
+    
+    
+    let text = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+    var timer : Timer!
+    var progressCounter : Float = 0
+    let duration : Float = 10.0
+    var progressIncrement : Float = 0
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setLabel()
+       
+    }
+
+    
+    @objc func showProgress(){
+        
+        if (progressCounter > 1.0) {
+            timer.invalidate()
+        }
+        
+        
+        progressBar.progress = progressCounter
+        progressCounter = progressCounter + progressIncrement
+        text.text = "\(Int(progressBar.progress * 100))%"
+    }
+    
+    
+    
+    func setLabel(){
+        text.center = view.center
+        text.textAlignment = .center
+        text.font = UIFont(name: "MarkerFelt-Wide", size: 25)
+        text.textColor = .white
+        view.addSubview(text)
+    }
+
+    
+    @IBAction func startBtnWasPressed(_ sender: Any) {
+        progressIncrement = 0.01
+        timer = Timer.scheduledTimer(timeInterval: 0.05, target: self, selector: #selector(showProgress), userInfo: nil, repeats: true)
+    }
+    
+
+}
+
---
+++ CircularProgressBar/View/ProgressBarView.swift
@@ -0,0 +1,85 @@
+//
+//  ProgressBarView.swift
+//  CircularProgressBar
+//
+//  Created by Massimiliano on 12/11/2019.
+//  Copyright © 2019 Massimiliano Bonafede. All rights reserved.
+//
+
+import UIKit
+
+class ProgressBarView: UIView {
+
+    
+    var bgPath : UIBezierPath!
+    var shapeLayer : CAShapeLayer!
+    var progressLayer : CAShapeLayer!
+    
+    var progress : Float = 0 {
+        willSet(newValue) {
+            progressLayer.strokeEnd = CGFloat(newValue)
+        }
+    }
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        bgPath = UIBezierPath()
+        self.simpleShape()
+    }
+    
+    
+    
+    
+    required init?(coder aDecoder : NSCoder) {
+        super.init(coder : aDecoder)
+        bgPath = UIBezierPath()
+        self.simpleShape()
+    }
+    
+    
+    
+    func simpleShape(){
+        
+        createCirclePath()
+        
+        createShapeLayer()
+        
+        createProgressLayer()
+    }
+    
+    
+    func createShapeLayer(){
+        shapeLayer = CAShapeLayer()
+        shapeLayer.path = bgPath.cgPath
+        shapeLayer.lineWidth = 20
+        shapeLayer.fillColor = UIColor.darkGray.cgColor
+        shapeLayer.strokeColor = UIColor.clear.cgColor
+        
+        self.layer.addSublayer(shapeLayer)
+    }
+    
+    
+    
+    func createProgressLayer(){
+        progressLayer = CAShapeLayer()
+        progressLayer.path = bgPath.cgPath
+        progressLayer.lineCap = CAShapeLayerLineCap.round
+        progressLayer.lineWidth = 15
+        progressLayer.fillColor = nil
+        progressLayer.strokeColor = UIColor.red.cgColor
+        progressLayer.strokeEnd = 0.0
+        
+        self.layer.addSublayer(progressLayer)
+    }
+    
+    
+    func createCirclePath(){
+        
+        let center = CGPoint(x: self.frame.size.width / 2, y: self.frame.size.height / 2)
+        
+        bgPath.addArc(withCenter: center, radius: 100, startAngle: 0 - CGFloat.pi / 2, endAngle: (3 * CGFloat.pi) / 2, clockwise: true)
+        
+        bgPath.close()
+    }
+}
